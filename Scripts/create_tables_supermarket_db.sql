ed7accacd6630b5d5a863244873e971f6ac152a6
Nicolas Soares <nicolassoares.santos@outlook.com>
2024-01-31 19:34:23 -0300

    Improvement in Draft Table Generation Script
    This commit improves the draft SQL script for generating tables in the database. Significant improvements were made to the definitions of the Produtos_comprados and Produtos_atacadao_gopiuva tables, including more comprehensive details and adjustments to foreign keys to ensure referential integrity. It should be noted that this is a draft and has not yet been applied to the production database, representing an advanced stage in preparation for future implementations.

--- Scripts/create_tables_supermarket_db.sql
+++ Scripts/create_tables_supermarket_db.sql
@@ -1,10 +1,40 @@
-CREATE TABLE SuperMarketDB.Produtos (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    codigo_de_barras TEXT NOT NULL UNIQUE,
+CREATE TABLE SuperMarketDB.produtos_comprados (
+    id_ordem INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_invoice TEXT,
+    codigo_produto TEXT NOT NULL,
     nome TEXT NOT NULL,
     marca TEXT,
-    categoria TEXT,
     preco REAL,
-    quantidade_em_estoque INTEGER,
+    quantidade_comprada INTEGER,
+    data_cadastro DATE DEFAULT CURRENT_DATE,
+    FOREIGN KEY (codigo_produto) REFERENCES SuperMarketDB.produtos(codigo_de_barras),
+    FOREIGN KEY (id_invoice) REFERENCES InvoicesDB.valor_comra(id_invoice)
+);
+
+CREATE TABLE MarketProductCatalogDB.produtos_atacadao_gopiuva (
+    codigo_produto INTEGER PRIMARY KEY,
+    codigo_de_barras TEXT NOT NULL,
+    id_mercado SERIAL NOT NULL,
+    data_cadastro DATE DEFAULT CURRENT_DATE,
+    FOREIGN KEY (codigo_de_barras) REFERENCES SuperMarketDB.produtos(codigo_de_barras),
+    FOREIGN KEY (id_mercado) REFERENCES SuperMarketDB.mercados(id_mercado)
+);
+
+CREATE TABLE SuperMarketDB.produtos (
+    codigo_de_barras TEXT PRIMARY KEY,
+    nome TEXT NOT NULL CHECK (LENGTH(nome) <= 100),
+    marca TEXT CHECK (LENGTH(marca) <= 50),
+    categoria TEXT CHECK (LENGTH(categoria) <= 50),
+    data_cadastro DATE DEFAULT CURRENT_DATE,
+);
+
+CREATE TABLE SuperMarketDB.mercados (
+    id_mercado SERIAL PRIMARY KEY,
+    nome_mercado TEXT NOT NULL,
+    endereco TEXT,
+    cidade TEXT,
+    estado TEXT,
+    cep TEXT,
+    telefone TEXT,
     data_cadastro DATE DEFAULT CURRENT_DATE
 );
